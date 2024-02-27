9df068c256d286131ceaff6435a635a458da27c8
Zoran Tica <zoran.tica@gmail.com>
2024-02-27 10:03:11 +0100

    Readme and example

--- README.md
+++ README.md
@@ -1,12 +1,63 @@
 # Various Oracle Database and Oracle APEX utilities
-In this repository You may find various Oracle Database and Oracle APEX utilities.
+In this repository You may find various Oracle Database and Oracle APEX utilities:
+
+- Oracle APEX Collections DML
 
 ## Oracle APEX Collections DML
 This utility provides You the functionality to execute DML operations directly on APEX collections from pure SQL. No need for using PL/SQL APEX_COLLECTION API.
 
-For example, You may populate the APEX collection with data from EMP table like this:
-
-
-
 Installation script and examples script can be found in the folder "apex_dml_collections".
+
+With this utility You may, for example, to populate the APEX collection with data from DEMO_CUSTOMERS table in the following way:
+
+```
+INSERT INTO apex_collections_dml (
+    collection_name, 
+    n001,  --customer_id 
+    c001,  --cust_first_name 
+    c002,  --cust_last_name 
+    c003,  --cust_street_address1 
+    c004,  --cust_street_address2 
+    c005,  --cust_city 
+    c006,  --cust_state
+    c007,  --cust_postal_code
+    c008,  --cust_email
+    c009,  --phone_number1
+    c010,  --phone_number2
+    c011,  --url
+    c012,  --tag
+    n002   --credit_limit
+)
+SELECT
+    'DEMO_CUSTOMERS',
+    customer_id, 
+    cust_first_name, 
+    cust_last_name, 
+    cust_street_address1, 
+    cust_street_address2, 
+    cust_city, 
+    cust_state, 
+    cust_postal_code, 
+    cust_email, 
+    phone_number1, 
+    phone_number2, 
+    url, 
+    tags,
+    credit_limit
+FROM 
+    demo_customers 
+;
+```
+
+Or You may update the collection and change the credit limit for repeat customers by 10% like this:
+
+```
+UPDATE apex_collections_dml
+SET 
+    n002 = n002 + (10 * n002 / 100)
+WHERE
+    c012 = 'REPEAT CUSTOMER'
+AND collection_name = 'DEMO_CUSTOMERS'
+;
+```
 
--- apex_dml_collections/example.sql
+++ apex_dml_collections/example.sql
@@ -1,3 +1,7 @@
+-------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------------
+--EXAMPLE 01
+
 --if needed, create an APEX session
 BEGIN
     apex_session.create_session (
@@ -10,7 +14,7 @@ END;
 
 
 --insert new records into the collection
---if collection does not exist it is going to be created automatically -> no need to create it previously
+--if collection does not exist no problem - it is going to be created automatically - no need to create it previously
 INSERT INTO apex_collections_dml (
     collection_name, 
     n001, 
@@ -79,3 +83,104 @@ WHERE collection_name = 'MY_COLL'
 ;
 
 
+
+-------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------------
+--EXAMPLE 02 - use data from DEMO_CUSTOMERS table (table needs to be installed first with the APEX demo app)
+
+
+--clear the collection
+DELETE apex_collections_dml
+WHERE collection_name = 'DEMO_CUSTOMERS'
+;
+
+commit;
+
+--populate the collection from DEMO_CUSTOMERS table
+INSERT INTO apex_collections_dml (
+    collection_name, 
+    n001,  --customer_id 
+    c001,  --cust_first_name 
+    c002,  --cust_last_name 
+    c003,  --cust_street_address1 
+    c004,  --cust_street_address2 
+    c005,  --cust_city 
+    c006,  --cust_state
+    c007,  --cust_postal_code
+    c008,  --cust_email
+    c009,  --phone_number1
+    c010,  --phone_number2
+    c011,  --url
+    c012,  --tag
+    n002   --credit_limit
+)
+SELECT
+    'DEMO_CUSTOMERS',
+    customer_id, 
+    cust_first_name, 
+    cust_last_name, 
+    cust_street_address1, 
+    cust_street_address2, 
+    cust_city, 
+    cust_state, 
+    cust_postal_code, 
+    cust_email, 
+    phone_number1, 
+    phone_number2, 
+    url, 
+    tags,
+    credit_limit
+FROM 
+    demo_customers 
+;
+
+commit;
+
+
+--check data
+SELECT *
+FROM apex_collections
+WHERE collection_name = 'DEMO_CUSTOMERS'
+;
+
+SELECT *
+FROM apex_collections_dml
+WHERE collection_name = 'DEMO_CUSTOMERS'
+;
+
+
+
+--update credit limit for repeat customers by 10%
+UPDATE apex_collections_dml
+SET 
+    n002 = n002 + (10 * n002 / 100)
+WHERE
+    c012 = 'REPEAT CUSTOMER'
+AND collection_name = 'DEMO_CUSTOMERS'
+;
+
+commit;
+
+
+
+--check data
+SELECT *
+FROM apex_collections
+WHERE collection_name = 'DEMO_CUSTOMERS'
+;
+
+SELECT *
+FROM apex_collections_dml
+WHERE collection_name = 'DEMO_CUSTOMERS'
+;
+
+
+
+--remove customers from the state "VA"
+DELETE apex_collections_dml
+WHERE 
+    collection_name = 'DEMO_CUSTOMERS'
+AND c006 = 'VA'
+;
+
+commit;
